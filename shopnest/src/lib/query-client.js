4f8b68f38666452ee1b855dd4bd74ed8dab483e3
w0264667-sys <w0264667@gmail.com>
2026-06-08 06:26:25 +0200

    Delete shopnest directory

--- shopnest/src/lib/query-client.js
+++
@@ -1,2 +0,0 @@
-import { QueryClient } from '@tanstack/react-query';
-export const queryClientInstance = new QueryClient({ defaultOptions: { queries: { staleTime: 60000 } } });
--- shopnest/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'dark') document.documentElement.classList.add('dark');
-else if (savedTheme === 'light') document.documentElement.classList.remove('dark');
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
