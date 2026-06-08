20be254deb4062c5968cc88dc28dc90ceb2eac3c
w0264667-sys <w0264667@gmail.com>
2026-06-08 05:20:06 +0200

    Update vite.config.js

--- shopnest/vite.config.js
+++ shopnest/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+
 export default defineConfig({
-  plugins: [react()],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+base: '/Chopnest/', // Nombre exacto de tu repositorio
+
+plugins: [react()],
+
+resolve: {
+alias: {
+'@': path.resolve(__dirname, './src'),
+},
+},
+
+build: {
+outDir: 'dist',
+},
 });
